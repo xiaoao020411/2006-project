abb849f05b0b9fb6dfab56352e3ee858f5e3aa2f
changshenao <1253689006@qq.com>
2021-01-15 19:35:57 +0800

    oneinstack

--- pdo.php
+++ pdo.php
@@ -5,7 +5,7 @@ function getPdo()
 {
     $host = "127.0.0.1";
     $user = "root";
-    $pass = "123456";
+    $pass = "oneinstack";
     $db = "2006";
     return new PDO("mysql:host=$host;dbname=$db", $user, $pass);
 }
